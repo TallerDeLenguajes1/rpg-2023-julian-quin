576e4b75cc3f1499aadf93346353e0ce65acb008
julian <jnsoretniuq1998@gmail.com>
2023-06-22 06:32:29 -0300

    agrego metodo(numAleat) y completo el metodo crearPersonaje

--- FabricaDePersonajes.cs
+++ FabricaDePersonajes.cs
@@ -1,14 +1,36 @@
-namespace protectoo
+//Genere una clase para poder crear personajes aleatorios que se llame facrica de personajes
+//Debe tener un método que retorne un personaje con sus respetivos datos y características cargadas.
+using Jugadores;
+namespace Fabrica
 {
-    class FabricaDePersonajes
+    
+    public class FabricaDePersonajes
     {
-        private static Personaje crearPersonaje()
+        public static Personaje crearPersonaje() //metodo
         {
-            var Personaje = new Personaje ();
-            return Personaje;
+            var NuevoPersonaje = new Personaje();
+            NuevoPersonaje.Nombre = Constantes.nombres1[NumeroAleatorio(0,20)];
+            NuevoPersonaje.Tipo = Constantes.tipo[NumeroAleatorio(0,2)];
+            NuevoPersonaje.Edad= NumeroAleatorio(0,301);
+            NuevoPersonaje.Fuerza= NumeroAleatorio(1,11);
+            NuevoPersonaje.Armadura=NumeroAleatorio(1,11);
+            NuevoPersonaje.Destreza = NumeroAleatorio(1,6);
+            NuevoPersonaje.Salud=100;
+            NuevoPersonaje.Nivel = NumeroAleatorio(1,11);
+            NuevoPersonaje.Velocidad = NumeroAleatorio(1,11);
+            return NuevoPersonaje;
         }
         
+        public static int NumeroAleatorio(int a, int b) //metodo
+        {
+            Random ValorAletorio = new Random();
+            return (ValorAletorio.Next(a,b));
+        }
+
+
+
     }
 
+   
 
 }
