30a69b44973c4b20f72ac03841188268bcbd03c9
julian <jnsoretniuq1998@gmail.com>
2023-06-22 06:35:03 -0300

    primeras lineas para usar las clases creando los personajes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,44 @@
-﻿//using protectoo;
+﻿using Jugadores;
+using Fabrica;
+using Json;
 internal class Program
 {
     private static void Main(string[] args)
-    {
+    { 
+        //--------------------BATALLA DE CEREBRITOS--------------------
         
+        string NombreJson = "Personajes.json";
+        var ListaPersonajes = new List <Personaje> ();
+
+        if (!PersonajesJson.Existe(NombreJson))
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var NewPersonaje = FabricaDePersonajes.crearPersonaje();
+                ListaPersonajes.Add(NewPersonaje);
+            }
+            PersonajesJson.GuardarPersonajes(ListaPersonajes,NombreJson);
+        }else {
+            ListaPersonajes = PersonajesJson.LeerPersonajes(NombreJson);
+        }
+        MostrarPersonajes(ListaPersonajes);
+        
+    }
+
+    private static void MostrarPersonajes(List <Personaje> ListaPersonaje)
+    {
+        Console.WriteLine("\t[ PERSONAJES]\n");
+        foreach (var personaje in ListaPersonaje)
+        {
+            Console.WriteLine("Nombre: " + personaje.Nombre);
+            Console.WriteLine("Edad: " + personaje.Edad);
+            Console.WriteLine("Tipo: " + personaje.Tipo);
+            Console.WriteLine("Destreza: " + personaje.Destreza);
+            Console.WriteLine("Armadura"+ personaje.Armadura);
+            Console.WriteLine("Nivel: " + personaje.Nivel);
+            Console.WriteLine("Salud: " + personaje.Salud);
+            Console.WriteLine("Velocidad: "+personaje.Velocidad + "\n");
+        }
+
     }
 }
