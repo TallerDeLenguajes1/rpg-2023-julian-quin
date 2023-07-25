921e7a4610dd55a19ab2f41f0327ef13ab7a1847
julian <jnsoretniuq1998@gmail.com>
2023-07-25 05:57:45 -0300

    agrego mas strings

--- strings.cs
+++ strings.cs
@@ -15,8 +15,31 @@ namespace Textos
    | |  | |\___,_\ /_\ _      //       | |     | \_/ || |   | | | |  /\| |
    /_\  | |           //_____//       .||`      `._,' | |   | | \ `-' /| |
         /_\           `------'        \ |              `.\  | |  `._,' /_\
-                                       \|                    `.\
+                                       \|       GAME         `.\
 ";
+     public static string presentacion = @"╔════════════════════════════════════════════════╗
+║                                                ║
+║  Bienvenida al Torneo de Hogwarts: El Desafío  ║
+║        de los Magos. En esta ocasión,          ║
+║ tendrán el honor de ser testigos de uno de los ║
+║ eventos más prestigiosos y emocionantes del    ║
+║ mundo de la magia.                             ║
+║                                                ║
+╚════════════════════════════════════════════════╝";
+     public static string panelInicio =
+@"╔═════════════════════════════════════════════╗
+║                                             ║
+║           (1) Iniciar Torneo                ║
+║                                             ║
+║           (2) Mostrar Participantes         ║  
+║                                             ║
+║           (3) Salir                         ║
+║                                             ║
+╚═════════════════════════════════════════════╝";
+
+
+
 
     }
+       
 }
