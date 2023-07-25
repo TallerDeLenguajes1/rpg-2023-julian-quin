db2af5680df60ad97150151927b733bf3fa247e1
julian <jnsoretniuq1998@gmail.com>
2023-07-25 05:57:05 -0300

    emcapsulo codigo para el tipo

--- FabricaDePersonajes.cs
+++ FabricaDePersonajes.cs
@@ -6,58 +6,66 @@ namespace Fabrica
 {
     public static class FabricaDePersonajes
     {
-        public static Personaje crearPersonaje(string nombre, int id, string apodo) //metodo
+        public static Personaje crearPersonaje(string? nombre, int id, string apodo) //metodo
 
         {
             var NuevoPersonaje = new Personaje();
-        
-            NuevoPersonaje.Nombre = nombre;
-            NuevoPersonaje.FechaNac = new DateTime(NumeroAleatorio(1723,2024),NumeroAleatorio(1,13),NumeroAleatorio(1,31));
-            NuevoPersonaje.Edad= edad(NuevoPersonaje.FechaNac.Year);
-            NuevoPersonaje.Fuerza= NumeroAleatorio(1,11);
-            NuevoPersonaje.Armadura=NumeroAleatorio(1,11);
-            NuevoPersonaje.Destreza = NumeroAleatorio(1,6);
-            NuevoPersonaje.Salud=100;
-            NuevoPersonaje.Nivel = NumeroAleatorio(1,11);
-            NuevoPersonaje.Velocidad = NumeroAleatorio(1,11);
-            NuevoPersonaje.Apodo=apodo;
-            NuevoPersonaje.Id = id;
 
-            if(id >= 1 && id <=9 || id >=12 && id <=14 )
+            NuevoPersonaje.Nombre = nombre;
+            NuevoPersonaje.FechaNac = new DateTime(NumeroAleatorio(1900, 1980), NumeroAleatorio(1, 13), NumeroAleatorio(1, 31));
+            NuevoPersonaje.Edad = edad(NuevoPersonaje.FechaNac.Year);
+            NuevoPersonaje.Fuerza = NumeroAleatorio(1, 11);
+            NuevoPersonaje.Armadura = NumeroAleatorio(1, 11);
+            NuevoPersonaje.Destreza = NumeroAleatorio(1, 6);
+            NuevoPersonaje.Salud = 100;
+            NuevoPersonaje.Nivel = NumeroAleatorio(1, 11);
+            NuevoPersonaje.Velocidad = NumeroAleatorio(1, 11);
+            NuevoPersonaje.Apodo = apodo;
+            NuevoPersonaje.Id = id;
+            NuevoPersonaje.Tipo=DeterminarTipo(id);
+
+            return NuevoPersonaje;
+        }
+
+        private static string DeterminarTipo(int id)
+        {
+            string Tipo;
+            if (id >= 1 && id <= 9 || id >= 12 && id <= 14)
             {
-                NuevoPersonaje.Tipo = "Estudiante";
-            } else {
-                if (id >= 15 && id <=18 || id ==20)
+                Tipo = "Estudiante";
+            }
+            else
+            {
+                if (id >= 15 && id <= 18 || id == 20)
                 {
-                    NuevoPersonaje.Tipo = "Profesor";
-                } else {
+                    Tipo = "Profesor";
+                }
+                else
+                {
                     if (id == 21 || id == 22)
                     {
-                        NuevoPersonaje.Tipo = "Mago Tenebroso";
-                    }else
+                        Tipo = "Mago Tenebroso";
+                    }
+                    else
                     {
-                        NuevoPersonaje.Tipo ="Mago Bondadoso";
+                        Tipo = "Mago Bondadoso";
                     }
                 }
             }
-           
-            return NuevoPersonaje;
+            return Tipo;
         }
-        
+
         public static int NumeroAleatorio(int a, int b) //metodo
         {
             Random ValorAletorio = new Random();
             return (ValorAletorio.Next(a,b));
         }
-        public static int edad (int anioNac)
+        public static int edad (int AnioNacimiento)
         {
             int anioActual = DateTime.Now.Year;
 
-            return (anioActual-anioNac);
+            return (anioActual-AnioNacimiento);
         } 
-
-
-
     }
 
    
