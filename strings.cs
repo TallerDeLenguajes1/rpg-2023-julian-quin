812a03779cfd43b6384725f92c5d52a540c670a6
julian <jnsoretniuq1998@gmail.com>
2023-07-26 05:23:20 -0300

    agrego mas texto de presentacion

--- strings.cs
+++ strings.cs
@@ -17,25 +17,64 @@ namespace Textos
         /_\           `------'        \ |              `.\  | |  `._,' /_\
                                        \|       GAME         `.\
 ";
-     public static string presentacion = @"╔════════════════════════════════════════════════╗
+     public static string presentacion1 = @"╔════════════════════════════════════════════════╗
 ║                                                ║
-║  Bienvenida al Torneo de Hogwarts: El Desafío  ║
-║        de los Magos. En esta ocasión,          ║
-║ tendrán el honor de ser testigos de uno de los ║
-║ eventos más prestigiosos y emocionantes del    ║
-║ mundo de la magia.                             ║
+║        ¡Bienvenida al Torneo de Hogwarts       ║
+║           El Desafío de los Magos!             ║     
 ║                                                ║
 ╚════════════════════════════════════════════════╝";
-     public static string panelInicio =
+public static string presentacion2 =
+        @"╔═════════════════════════════════════════════════╗
+║                                                 ║
+║  En esta emocionante aventura, tendrás el honor ║
+║  de convertirte en el encargado de dirigir el   ║
+║  destino de 16 valientes personajes mágicos.    ║
+║                                                 ║
+╚═════════════════════════════════════════════════╝";
+public static string presentacion3 =
+        @"╔═════════════════════════════════════════════════╗
+║                                                 ║
+║  Deberás guiar a los 16 personajes a través de  ║
+║  una serie de combates épicos, en los cuales    ║
+║  deberán demostrar sus habilidades mágicas,     ║
+║  astucia y valentía para avanzar en el torneo.  ║
+║  Solo los más fuertes y habilidosos pasarán a   ║
+║  las siguientes rondas: octavos de final,       ║
+║  cuartos de final, semifinales y final.         ║
+║                                                 ║
+╚═════════════════════════════════════════════════╝";
+public static string presentacion4 = @"╔═════════════════════════════════════════════════╗
+║                                                 ║
+║      ¡Podrás hacerlo aún más emocionante        ║
+║      realizando apuestas junto a tus amigos!    ║
+║        ¿Cuál es tu personaje favorito?          ║
+║                                                 ║
+╚═════════════════════════════════════════════════╝";
+     public static string panelInicio1 =
 @"╔═════════════════════════════════════════════╗
 ║                                             ║
-║           (1) Iniciar Torneo                ║
+║          (1) Iniciar Torneo                 ║
 ║                                             ║
-║           (2) Mostrar Participantes         ║  
+║          (2) Mostrar Participantes          ║  
 ║                                             ║
-║           (3) Salir                         ║
+║          (3) Apuestas                       ║
+║                                             ║
+║          (4) Salir                          ║
 ║                                             ║
 ╚═════════════════════════════════════════════╝";
+ public static string panelInicio2 =
+@"╔═════════════════════════════════════════════╗
+║                                             ║
+║          (1) Jugar de Nuevo                 ║
+║                                             ║
+║          (2) Ver Ganador                    ║  
+║                                             ║
+║          (3) Apuestas                       ║
+║                                             ║
+║          (4) Salir                          ║
+║                                             ║
+╚═════════════════════════════════════════════╝";
+
 
 
 
